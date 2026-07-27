c363ba4495d441c815de3f6925d55e18d6378f0b
skillsstackpro <rohankataria48@gmail.com>
2026-07-27 23:38:08 +0500

    Add SonarCloud workflow

--- src/main/java/com/example/demo/service/JournalEntryService.java
+++ src/main/java/com/example/demo/service/JournalEntryService.java
@@ -60,7 +60,7 @@ public class JournalEntryService {
                 journalEntryRepository.deleteById(id);
             }
         }catch(Exception e){
-            System.out.println(e);
+            log.error("Error ",e);
             throw new RuntimeException("An error occured while deleting the entry. ",e);
         }
         return removed;
